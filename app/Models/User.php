dcf8811f75656f9ddf1a2c6d7264457bc113d118
rzayyanr <rzayyanrizka@gmail.com>
2025-12-03 15:34:59 +0700

    woi

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,6 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-    // nyoba push aja
     /**
      * The attributes that are mass assignable.
      *
